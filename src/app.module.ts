c4b1eaa5f4a2a85255c2c3c410c2d325481d156d
Rodrigo Lebuy <rodrigo.lebuy@gmail.com>
2025-01-31 18:02:27 -0300

    viernes tarde

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,20 @@ import { Mongoose } from 'mongoose';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfiguration } from './config/app.config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load:[EnvConfiguration],
+    }),
+
     ServeStaticModule.forRoot({
       rootPath:  join(__dirname,'..','public'),
     }),
-
-    MongooseModule.forRoot('mongodb://lobo.local:27017/nest-pokemon'),
+    //MongooseModule.forRoot('mongodb://lobo.local:27017/nest-pokemon'),
+    MongooseModule.forRoot(process.env.MONGODB || ''),
 
     PokemonModule,
 
@@ -22,4 +28,9 @@ import { SeedModule } from './seed/seed.module';
     SeedModule
   ],
 })
-export class AppModule {}
+export class AppModule {
+
+  constructor(){
+    console.log(process.env);
+  }
+}
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -12,7 +12,9 @@ export class PokemonService {
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
-  ){}
+  ){
+    console.log(process.env.DEFAULT_LIMIT)
+  }
 
 
   async create(createPokemonDto: CreatePokemonDto) {
@@ -32,7 +34,7 @@ export class PokemonService {
     const {limit=10 ,offset=0} = paginationDto;
 
     return this.pokemonModel.find()
-    .limit(limit).skip(offset).sort({no:1}).select('-__v');
+    .limit(limit).skip(offset).sort({no:1}).select('-__');
     //return `This action returns all pokemon`;
   }
 
